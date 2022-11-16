69ff70c56ca1eba051b72263a90efb6e1dbf58ae
timtimtimm <carli197108@gmail.com>
2022-11-16 21:59:41 +0300

    rework

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import s from './Header.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { onDictionarySwitch } from './../store/sliceDictionary';
+import { onDictionarySwitch, togleStatusTable } from './../store/sliceDictionary';
 
 const Header = () => {
 
     const togle = useSelector(state => state.dictionary.dictionarySwitch);
     const dispatch = useDispatch();
 
+    const setTogleStatusTable = (status) => {
+        dispatch(togleStatusTable(status));
+    }
+
     return (
         <div className={s.header}>
+            <div className={s.buttons}>
+                <button onClick={() => setTogleStatusTable('add')} >Все слова</button>
+                <button onClick={() => setTogleStatusTable('remember')}>Выученные слова</button>
+                <button onClick={() => setTogleStatusTable('not remember')}>Невыученные слова</button>
+            </div>
             <h1> Учим иностранные слова</h1>
-            {togle ? <button onClick={() => dispatch(onDictionarySwitch())}> Учить слова</button>
-                : <button onClick={() => dispatch(onDictionarySwitch())}> Редактировать словарь</button>}
-
-
+            {togle ? <button className={s.togle} onClick={() => dispatch(onDictionarySwitch())}> Учить слова</button>
+                : <button className={s.togle} onClick={() => dispatch(onDictionarySwitch())}> Редактировать словарь</button>}
         </div>
     )
 }
